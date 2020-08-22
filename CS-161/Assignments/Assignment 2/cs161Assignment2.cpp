a6d136183a49617b0e2ae106319f9cd1f649f378
Pranaw Bajracharya <bajrachp@oregonstate.edu>
2020-08-22 02:09:41 -0700

    Moved program information to README.txt

--- CS-161/Assignments/Assignment 2/cs161Assignment2.cpp
+++ CS-161/Assignments/Assignment 2/cs161Assignment2.cpp
@@ -1,18 +1,3 @@
-/*************************************************************************************
- * AUTHOR:
- * 	Pranaw Bajracharya
- *
- * PROGRAM NAME:
- * 	Helper Function Library
- *
- * INPUTS:
- *	A prompted string from the user
- *
- * OUTPUTS:
- *	(Only in individual functions)
- *
- ************************************************************************************/
-
 #include <iostream>
 #include <string>
 #include <cmath>
