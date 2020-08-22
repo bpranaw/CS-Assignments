80f9a5c9017e0d13a41fe2901adf8e79c4a3159f
Pranaw Bajracharya <bajrachp@oregonstate.edu>
2020-08-22 05:34:55 -0700

    Moving program information to README.txt

--- CS-162/Assignments/Assignment 3/main.cpp
+++ CS-162/Assignments/Assignment 3/main.cpp
@@ -1,30 +1,3 @@
-/*************************************************************************************************************************************************************************************
- * Program Name: Zoo Game
- *
- * Author: Pranaw Bajracharya
- *
- * Description: 
- *
- * 	This program is a text based game. The game is as follows:
- *
- * 	You play as a zookeepr for Krusty Krab Zoo. Your goal is to make the zoo as much
- * 	money as possible. You will start with $50,000 at the start. Each turn will consist of:
- * 		1) A month passing
- * 		2) The recieval of Monthly Revenue for each animal
- * 		3) A special Event
- * 		4) The option to buy animals
- * 		5) The option to sell animals
- * 		6) The requirement of buying next month's animal feed
- * 			There are some choices of feed
- * 	
- * 	If you loose too much money (the zoo's balance is <0). you will become bankrupt and the game will end.
- * 		
- * 		
- * Inputs: N/A
- * Outputs: N/A 
- * 
- ************************************************************************************************************************************************************************************/
-
 #include "zoo.h"
 #include "animal.h"
 #include "monkey.h"
