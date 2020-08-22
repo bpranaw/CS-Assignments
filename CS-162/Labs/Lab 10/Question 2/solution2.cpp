974382882f439abfa1ea74d5e5c25f106130d720
Pranaw Bajracharya <bpranaw@gmail.com>
2020-08-22 06:23:08 -0700

    Added Lab 10 Question 2 files

---
+++ CS-162/Labs/Lab 10/Question 2/solution2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "list.h"
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+	//Type your answer here:
+	
+
+	ListNode* current_node = head;
+
+	ListNode* dIndex = head;
+
+	if
+
+}
