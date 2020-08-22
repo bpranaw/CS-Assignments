d050d4320354064c78cc3a0391fa5e7e913fc642
Pranaw Bajracharya <bajrachp@oregonstate.edu>
2020-08-22 02:16:40 -0700

    Moving program information to README.txt

--- CS-161/Assignments/Assignment 5/cs161Assignment5.cpp
+++ CS-161/Assignments/Assignment 5/cs161Assignment5.cpp
@@ -1,36 +1,3 @@
-/*************************************************************************************************************************************************************************************
- * Author:
- * 	Pranaw Bajracharya
- * 
- * Program Name:
- * 	Connect Four
- *
- * Description:
- * 	This program is a CONNECT FOUR game and as such follows official CONNECT FOUR rules as seen here:
- * 	This game has two game types:
- * 		1) HUMAN vs COMPUTER
- * 			The player can choose between two differ AI
- * 				Dumb - The dumb AI will choose legal pieces at random
- * 				Smart - The smart AI will choose between a selection of options that relate to the 
- * 					last column that the user selected
- * 					It will never allow the user to connect four vertically
- * 		2) HUMAN vs HUMAN
- * 			Two players can play against each other
- *
- * 	The user can play the game as many times as they like. If they finish a game, the will be prompted if they want to play again.
- * 	If they decide to play agian, the game board will be reset to the specific parameters they entered through the command prompt and the game restarted.
- * 
- *
- * Iputs:
- *	Number of Players - Correct values (1 or 2)
- *
- *	Number of Rows - Correct values: (4-20)
- *
- *	Number of Columns - Correct values (4-20)
- * Outputs:
- *	None
- *
- ************************************************************************************************************************************************************************************/
 #include <iostream>
 #include <cstdlib> 
 #include <cstring>
